1350d863cab991bdbd13eba121e01441c7117064
CReies <ccreyesmolina@gmail.com>
2021-12-16 19:58:27 -0500

    bug fixes and Product Controller created

--- src/main/java/com/mintic/ciclo4/controller/UserController.java
+++ src/main/java/com/mintic/ciclo4/controller/UserController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -48,19 +49,19 @@ public class UserController {
 
   @PostMapping("/new")
   @ResponseStatus(HttpStatus.CREATED)
-  public User save(User user) {
+  public User save(@RequestBody User user) {
     return userService.save(user);
   }
 
   @PutMapping("/update")
   @ResponseStatus(HttpStatus.CREATED)
-  public User update(User user) {
+  public User update(@RequestBody User user) {
     return userService.save(user);
   }
 
   @DeleteMapping("/{id}")
   @ResponseStatus(HttpStatus.NO_CONTENT)
-  public void delete(int id) {
+  public void delete(@PathVariable int id) {
     userService.delete(id);
   }
 
