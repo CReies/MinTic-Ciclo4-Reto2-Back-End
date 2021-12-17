8aed1d60a4dbb2a06e441c9e7666249d992d7101
CReies <ccreyesmolina@gmail.com>
2021-12-17 17:20:42 -0500

    bug fixes in ProductController

--- src/main/java/com/mintic/ciclo4/controller/ProductController.java
+++ src/main/java/com/mintic/ciclo4/controller/ProductController.java
@@ -8,18 +8,18 @@ import com.mintic.ciclo4.service.ProductService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/api/cleaningprod")
 @CrossOrigin("*")
 public class ProductController {
